1a48aa7311d6722c1cba6265ddea2c95f66e6c32
JasonOnes <jasonr.jones14@gmail.com>
2018-01-04 14:49:16 -0800

    list persist

--- lib_select.py
+++ lib_select.py
@@ -1,17 +1,31 @@
+import sqlite3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import random
 
 
-def find_subject_library():
-    # gives a random subject.section number in index for day's review
+def create_lib_list():
+    # creates a light db for the list of sections of the python library index
+    db = sqlite3.connect('lib-list')
+    cursor = db.cursor()
+    
+    cursor.execute(""" DROP TABLE lib""")
+    db.commit()
+    cursor.execute('''
+        CREATE TABLE lib(id INTEGER PRIMARY KEY, section TEXT,
+                        done INTEGER)''')
+    # note no BOOLEAN Data type in sqlite hence INTEGER(0,1)
+    db.commit()
+    
+    # official python page for built-ins
     html = urlopen("https://docs.python.org/3/library/index.html")
     soupy_page = BeautifulSoup(html.read(), "html.parser")
+    # since I'm web scraping a static official page I just used html tags
     index_body = soupy_page.findAll("", {"class":"reference internal"})
 
     nums = []
     items_to_go = []
     for item in index_body:
+        # didn't bother with regex since just doing this once to populate db
         nums.append(item.get_text()[:5])
     for num in nums:
         for c in num:
@@ -20,9 +34,38 @@ def find_subject_library():
 
     fresh_index = [item for item in nums if item not in items_to_go]
 
-    today_lesson = random.choice(fresh_index)
-    print(today_lesson)
+    for item in fresh_index:
+        cursor.execute("""INSERT INTO lib(section, done) VALUES(?, ?)""", (item, 0))
 
+    print("db populated, ")
+    db.commit()
+    print("and created.")
    
+
+
+def find_subject_library():
+    # gives a random subject.section number in index for day's review
+    db = sqlite3.connect('lib-list')
+    cursor = db.cursor()
+
+    today_lesson = cursor.execute("""SELECT section FROM lib WHERE done==0 ORDER BY RANDOM() LIMIT 1""")
+    # today_lesson currently a Cursor object
+    lib_num_tuple = today_lesson.fetchone() # now tuple
+    lib_num = ''.join(lib_num_tuple) # now string
+    if lib_num[-1] == '.':
+        print(lib_num[:-1])
+    else:
+        print(lib_num)
+
+    cursor.execute('''UPDATE lib SET done = ? WHERE section = ? ''', (1, lib_num))
+    db.commit()
+
+    count_curs = cursor.execute(''' SELECT COUNT (*) FROM lib WHERE done == 0''')
+    count = str(count_curs.fetchone())
+    print("db updated, lesson marked as done")
+    print("Only {} more built-ins to review!".format(count))
+    
+
+#create_lib_list()
 find_subject_library()
 
